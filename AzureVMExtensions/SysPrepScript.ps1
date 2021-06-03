34aad4002e509d763dfb6242a41b2959909aa30e
NielsKok <niels@nko-it.nl>
2021-06-03 15:14:48 +0200

    SysprepScript

--- AzureVMExtensions/SysPrepScript.ps1
+++ AzureVMExtensions/SysPrepScript.ps1
@@ -1,22 +1,2 @@
-if (!(Test-Path C:\ProgramData\DevOpsInstallFolder)) {
-    New-Item C:\ProgramData\DevOpsInstallFolder -ItemType Directory
-}
 
-if (!(Test-Path C:\ProgramData\DevOpsInstallFolder\Scripts)) {
-    New-Item C:\ProgramData\DevOpsInstallFolder\Scripts -ItemType Directory
-}
-
-$SysPrepScriptcontents = {
-
-    if( Test-Path C:\windows\system32\Sysprep\unattend.xml ){ Remove-Item C:\windows\system32\Sysprep\unattend.xml -Force}
-        C:\Windows\System32\Sysprep\Sysprep.exe /oobe /generalize /quiet /shutdown;
-        while($true) { $imageState = Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Setup\State | Select-Object ImageState; if($imageState.ImageState -ne 'IMAGE_STATE_GENERALIZE_RESEAL_TO_OOBE') { Write-Output $imageState.ImageState; Start-Sleep -s 10  } else { break } }
-}
-$Sysprepscriptpath = 'C:\ProgramData\DevOpsInstallFolder\Scripts\Sysprepscript.ps1'
-
-if (!(Test-Path $Sysprepscriptpath)) {
-    New-Item $Sysprepscriptpath -ItemType File
-
-    Add-Content -Path $Sysprepscriptpath -Value $SysPrepScriptcontents
-}
-
+Start-Process -FilePath C:\Windows\System32\Sysprep\Sysprep.exe -ArgumentList '/generalize /oobe /quiet /quit'  -Wait
