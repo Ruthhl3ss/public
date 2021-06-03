2da2ad5edca48b946cc76fdbf74138defa24b327
NielsKok <niels@nko-it.nl>
2021-06-03 12:34:24 +0200

    Typo

--- AzureVMExtensions/DevOpsBuildPrereq.ps1
+++ AzureVMExtensions/DevOpsBuildPrereq.ps1
@@ -13,7 +13,7 @@ $SysPrepScriptcontents = {
         C:\Windows\System32\Sysprep\Sysprep.exe /oobe /generalize /quiet /quit;
         while($true) { $imageState = Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Setup\State | Select-Object ImageState; if($imageState.ImageState -ne 'IMAGE_STATE_GENERALIZE_RESEAL_TO_OOBE') { Write-Output $imageState.ImageState; Start-Sleep -s 10  } else { break } }
 }
-$Sysprepscriptpath = C:\ProgramData\DevOpsInstallFolder\Scripts\Sysprepscript.ps1
+$Sysprepscriptpath = 'C:\ProgramData\DevOpsInstallFolder\Scripts\Sysprepscript.ps1'
 
 if (!(Test-Path $Sysprepscriptpath)) {
     New-Item $Sysprepscriptpath -ItemType File
