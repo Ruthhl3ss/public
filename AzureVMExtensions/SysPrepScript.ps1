86bf810be76e70899cd5bd29b89255ce07ebe59f
NielsKok <niels@nko-it.nl>
2021-06-03 15:52:31 +0200

    ShutdownEdit

--- AzureVMExtensions/SysPrepScript.ps1
+++ AzureVMExtensions/SysPrepScript.ps1
@@ -1,2 +1,2 @@
 
-Start-Process -FilePath C:\Windows\System32\Sysprep\Sysprep.exe -ArgumentList '/generalize /oobe /quiet /quit'  -Wait
+Start-Process -FilePath C:\Windows\System32\Sysprep\Sysprep.exe -ArgumentList '/generalize /oobe /shutdown /quiet'  -Wait
