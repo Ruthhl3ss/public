22e4340d89c02f891c333c150da057e79ac91c36
NielsKok <niels@nko-it.nl>
2021-06-10 16:50:08 +0200

    Update

--- AzureVMExtensions/DownloadInstallAzureCLI.ps1
+++ AzureVMExtensions/DownloadInstallAzureCLI.ps1
@@ -1,3 +1,3 @@
-Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi; Start-Sleep -Seconds 5; az extension add --upgrade -n azure-devops
+Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi; Start-Sleep -Seconds 5; az extension add --name azure-devops
 
 #az config set extension.use_dynamic_install=yes_without_prompt
