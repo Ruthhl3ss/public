a7fd98e6edb4093e5050553724a336e1b6297d57
NielsKok <niels@nko-it.nl>
2021-06-10 13:54:53 +0200

    Update

--- AzureVMExtensions/DownloadInstallAzureCLI.ps1
+++ AzureVMExtensions/DownloadInstallAzureCLI.ps1
@@ -1,3 +1,5 @@
 Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
 
+Start-Sleep -Seconds 5
+
 az config set extension.use_dynamic_install=yes_without_prompt
