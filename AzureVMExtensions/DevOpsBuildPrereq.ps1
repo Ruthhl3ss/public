082d0ac907671e89ccdb185d082fa5c8e506cb54
NielsKok <niels@nko-it.nl>
2021-06-10 12:33:08 +0200

    NewFile

--- AzureVMExtensions/DevOpsBuildPrereq.ps1
+++ AzureVMExtensions/DevOpsBuildPrereq.ps1
@@ -7,17 +7,17 @@ if (!(Test-Path C:\ProgramData\DevOpsInstallFolder\Scripts)) {
     New-Item C:\ProgramData\DevOpsInstallFolder\Scripts -ItemType Directory
 }
 
-$SysPrepScriptcontents = {
+$ArtifactsDownloadScriptcontents = {
 
-if( Test-Path C:\windows\system32\Sysprep\unattend.xml ){ Remove-Item C:\windows\system32\Sysprep\unattend.xml -Force}
-C:\Windows\System32\Sysprep\Sysprep.exe /oobe /generalize /quiet /shutdown;
-while($true) { $imageState = Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Setup\State | Select-Object ImageState; if($imageState.ImageState -ne 'IMAGE_STATE_GENERALIZE_RESEAL_TO_OOBE') { Write-Output $imageState.ImageState; Start-Sleep -s 10  } else { break } }
+    
+
+
 }
-$Sysprepscriptpath = 'C:\ProgramData\DevOpsInstallFolder\Scripts\Sysprepscript.ps1'
+$ArtifactsDownloadScriptcontents = 'C:\ProgramData\DevOpsInstallFolder\Scripts\Sysprepscript.ps1'
 
-if (!(Test-Path $Sysprepscriptpath)) {
-    New-Item $Sysprepscriptpath -ItemType File
+if (!(Test-Path $ArtifactsDownloadScriptcontents)) {
+    New-Item $ArtifactsDownloadScriptcontents -ItemType File
 
-    Add-Content -Path $Sysprepscriptpath -Value $SysPrepScriptcontents
+    Add-Content -Path $ArtifactsDownloadScriptcontents -Value $SysPrepScriptcontents
 }
 
