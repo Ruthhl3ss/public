f8be005baf4abec2489f8dde10bac53559b199e9
niels@nko-it.nl <niels@nko-it.nl>
2021-06-15 13:10:30 +0200

    ArtifactInstllation

--- AzureVMExtensions/ArtifactInstallation.ps1
+++ AzureVMExtensions/ArtifactInstallation.ps1
@@ -2,56 +2,54 @@
 
 
 
-    param (
-    [Parameter(Mandatory = $True, Position = 1, ValueFromPipeline = $False)]
-    [string]$AzLoginname,
-    [Parameter(Mandatory = $True, Position = 2, ValueFromPipeline = $False)]
-    [string]$AzPassword,
-    [Parameter(Mandatory = $True, Position = 3, ValueFromPipeline = $False)]
-    [string]$PackageName,
-    [Parameter(Mandatory = $True, Position = 4, ValueFromPipeline = $False)]
-    [string]$DOOrganizationname,
-    [Parameter(Mandatory = $True, Position = 5, ValueFromPipeline = $False)]
-    [string]$DOProject,
-    [Parameter(Mandatory = $True, Position = 6, ValueFromPipeline = $False)]
-    [string]$Scope,
-    [Parameter(Mandatory = $True, Position = 7, ValueFromPipeline = $False)]
-    [string]$Feed,
-    [Parameter(Mandatory = $True, Position = 8, ValueFromPipeline = $False)]
-    [string]$ArtifactName,
-    [Parameter(Mandatory = $True, Position = 9, ValueFromPipeline = $False)]
-    [string]$Version
-    )
+param (
+[Parameter(Mandatory = $True, Position = 1, ValueFromPipeline = $False)]
+[string]$AzLoginname,
+[Parameter(Mandatory = $True, Position = 2, ValueFromPipeline = $False)]
+[string]$AzPassword,
+[Parameter(Mandatory = $True, Position = 3, ValueFromPipeline = $False)]
+[string]$PackageName,
+[Parameter(Mandatory = $True, Position = 4, ValueFromPipeline = $False)]
+[string]$DOOrganizationname,
+[Parameter(Mandatory = $True, Position = 5, ValueFromPipeline = $False)]
+[string]$DOProject,
+[Parameter(Mandatory = $True, Position = 6, ValueFromPipeline = $False)]
+[string]$Scope,
+[Parameter(Mandatory = $True, Position = 7, ValueFromPipeline = $False)]
+[string]$Feed,
+[Parameter(Mandatory = $True, Position = 8, ValueFromPipeline = $False)]
+[string]$ArtifactName,
+[Parameter(Mandatory = $True, Position = 9, ValueFromPipeline = $False)]
+[string]$Version
+)
 
-    $Path = 'C:\ProgramData\DevOpsInstallFolder\'+$PackageName
+$Path = 'C:\ProgramData\DevOpsInstallFolder\'+$PackageName
+    
+#CreationOfFolders
+If (!(Test-Path 'C:\ProgramData\DevOpsInstallFolder')){
+    New-Item 'C:\ProgramData\DevOpsInstallFolder' -ItemType Directory
+}
 
-    #CreationOfFolders
-    If (!(Test-Path 'C:\ProgramData\DevOpsInstallFolder')){
-        New-Item 'C:\ProgramData\DevOpsInstallFolder' -ItemType Directory
-    }
-
-    If (!(Test-Path $Path)){
-        New-Item $Path -ItemType Directory
-    }
-
-    #$Pw = ConvertTo-SecureString $AzPassword -AsPlainText -Force
-
-    az extension add --name azure-devops --yes
-
-    az login --allow-no-subscriptions -u $AzLoginname -p $AzPassword 
-
-    az artifacts universal download --organization $DOOrganizationname --project $DOProject --scope $Scope --feed $Feed --version $Version --name $ArtifactName --path $path
-
-    $installScript = Get-ChildItem $Path -Filter *.ps1 
-
-    If (Test-Path $installScript.Fullname ){
-
-        .$installScript.Fullname
-    }
-    Else {
-
-        Write-Host "Install Script not available"
-    }
+If (!(Test-Path $Path)){
+    New-Item $Path -ItemType Directory
+}
 
 
-#InstallArtifact -AzLoginname $AzLoginname -AzPassword $AzPassword -PackageName $PackageName -DOOrganizationname $DOOrganizationname -DOProject $DOProject -Scope $Scope -Feed $Feed -ArtifactName $ArtifactName -Version $Version
+
+az extension add --name azure-devops --yes
+
+az login --allow-no-subscriptions -u $AzLoginname -p $AzPassword 
+
+az artifacts universal download --organization $DOOrganizationname --project $DOProject --scope $Scope --feed $Feed --version $Version --name $ArtifactName --path $path
+
+$installScript = Get-ChildItem $Path -Filter *.ps1 
+
+If (Test-Path $installScript.Fullname ){
+
+    .$installScript.Fullname
+}
+Else {
+
+    Write-Host "Install Script not available"
+}
+
