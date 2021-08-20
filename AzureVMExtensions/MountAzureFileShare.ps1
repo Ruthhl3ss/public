44d16d6105fcf28e5c1fa8af901d5b1825e91673
NKO <niels@nko-it.nl>
2021-08-20 09:53:38 +0200

    UpdatesVariables

--- AzureVMExtensions/MountAzureFileShare.ps1
+++ AzureVMExtensions/MountAzureFileShare.ps1
@@ -1,4 +1,3 @@
-##
 
 [CmdletBinding()]
 param (
@@ -19,12 +18,12 @@ $storageAccountKey = Get-AzStorageAccountKey -ResourceGroupName $storageaccountr
 
 Write-Host ($storageAccountKey).Value[0]
 
-$connectTestResult = Test-NetConnection -ComputerName $storageaccountrgname.file.core.windows.net -Port 445
+$connectTestResult = Test-NetConnection -ComputerName "$($storageaccountname).file.core.windows.net" -Port 445
 if ($connectTestResult.TcpTestSucceeded) {
     # Save the password so the drive will persist on reboot
-    cmd.exe /C "cmdkey /add:`"owdshared.file.core.windows.net`" /user:`"localhost\owdshared`" /pass:$(($storageAccountKey).Value[0])"
+    cmd.exe /C "cmdkey /add:`"$($storageaccountname).file.core.windows.net`" /user:`"localhost\$($storageaccountname)`" /pass:$(($storageAccountKey).Value[0])"
     # Mount the drive
-    New-PSDrive -Name Z -PSProvider FileSystem -Root "\\owdshared.file.core.windows.net\$($filesharename)" -Persist
+    New-PSDrive -Name Z -PSProvider FileSystem -Root "\\$($storageaccountname).file.core.windows.net\$($filesharename)" -Persist
 } else {
     Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
 }
