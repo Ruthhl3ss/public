296c1f60842e4073aaeadc1a3b87af1a49ef38f6
NKO <niels@nko-it.nl>
2021-08-20 09:16:27 +0200

    NewFile

---
+++ AzureVMExtensions/MountAzureFileShare.ps1
@@ -0,0 +1,37 @@
+##
+
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $storageaccountrgname,
+    [Parameter()]
+    [string]
+    $storageaccountname,
+    [Parameter()]
+    [string]
+    $filesharename
+
+)
+
+## Get StorageAccountKey
+$storageAccountKey = Get-AzStorageAccountKey -ResourceGroupName $storageaccountrgname -AccountName $storageaccountname
+
+Write-Host ($storageAccountKey).Value[0]
+
+$connectTestResult = Test-NetConnection -ComputerName $storageaccountrgname.file.core.windows.net -Port 445
+if ($connectTestResult.TcpTestSucceeded) {
+    # Save the password so the drive will persist on reboot
+    cmd.exe /C "cmdkey /add:`"owdshared.file.core.windows.net`" /user:`"localhost\owdshared`" /pass:$(($storageAccountKey).Value[0])"
+    # Mount the drive
+    New-PSDrive -Name Z -PSProvider FileSystem -Root "\\owdshared.file.core.windows.net\$($filesharename)" -Persist
+} else {
+    Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
+}
+
+If (Test-Path Z:\Office){
+    Write-Host "Z: Drive Mapped" -ForegroundColor Green
+}
+Else {
+    Write-Host "Drive not mapped" -ForegroundColor Red
+}
