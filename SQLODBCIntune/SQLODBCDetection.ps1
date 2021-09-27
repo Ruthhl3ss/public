28a472f6d4bd8123461a1d686b72cd9ae065bc41
NKO <niels@nko-it.nl>
2021-09-27 14:41:27 +0200

    ChangedDetectionMethod

--- SQLODBCIntune/SQLODBCDetection.ps1
+++ SQLODBCIntune/SQLODBCDetection.ps1
@@ -2,15 +2,15 @@
 $InstalledApplications = Get-WmiObject Win32_Product
 
 if ($InstalledApplications.IdentifyingNumber -contains '{6CD9E9ED-906D-4196-8DC3-F987D2F6615F}') {
-    Write-Log 'Microsoft Visual C++ 2017 X64 Runtime installed succesfully' -Level info
+    Write-Host 'Microsoft Visual C++ 2017 X64 Runtime installed succesfully'
 }
 Else {
-    Write-Log 'Microsoft Visual C++ 2017 X64 Runtime installation not found' -Level Error
+    Write-Error 'Microsoft Visual C++ 2017 X64 Runtime installation not found'
 }
 
 if ($InstalledApplications.identifyingNumber -contains '{7453C0F5-03D5-4412-BB8F-360574BE29AF}') {
-    Write-Log 'Microsoft ODBC Driver 17 installed succesfully' -Level info
+    Write-Host 'Microsoft ODBC Driver 17 installed succesfully'
 }
 Else {
-    Write-Log 'Microsoft ODBC Driver 17 installation not found' -Level Error
+    Write-Error 'Microsoft ODBC Driver 17 installation not found'
 }
