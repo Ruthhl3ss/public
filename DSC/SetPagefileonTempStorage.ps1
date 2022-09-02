5781ead3729580111b41e271f74235a1025f66a2
nkoadmin@7l4l1k.onmicrosoft.com <nkoadmin@7l4l1k.onmicrosoft.com>
2022-09-02 13:26:10 +0200

    NewScript

---
+++ DSC/SetPagefileonTempStorage.ps1
@@ -0,0 +1,13 @@
+#Get LogicDisks for machine with Temporary Disks
+$LogicalDisks = Get-WmiObject Win32_LogicalDisk | Where-Object VolumeName -EQ 'Temporary Storage'
+
+#Set Pagefile on Temp disk if exists
+If ($LogicalDisks){
+    
+    Write-Host "Setting PageFile"
+    $pagefileset = Get-WmiObject win32_pagefilesetting | Where-Object{$_.caption -like "$($LogicalDisks.DeviceID)*"}
+    $pagefileset.InitialSize = 6144
+    $pagefileset.MaximumSize = 24576
+    $pagefileset.Put() | Out-Null
+
+}
