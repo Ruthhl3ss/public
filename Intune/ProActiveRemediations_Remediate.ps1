e855f76d18ce0802939a762d717035aa74127e97
NKO <nkoadmin@7l4l1k.onmicrosoft.com>
2022-09-04 13:32:09 +0200

    UpdatedScripts

--- Intune/ProActiveRemediations_Remediate.ps1
+++ Intune/ProActiveRemediations_Remediate.ps1
@@ -5,8 +5,10 @@ $Test = Test-Path -Path $Path -ErrorAction SilentlyContinue
 If ($Test -eq $False) {
     try {
         New-Item -Path $Path -ItemType File
+        Exit 0
     }
     catch {
         Write-Error $_
+        Exit 1
     }
 }
