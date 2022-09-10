47fa9cd6ede244884fd622ee5865d084699f8319
NielsKok <nkoadmin@7l4l1k.onmicrosoft.com>
2022-09-10 13:16:54 +0200

    ChangedScript

--- DSC/SetPagefileonTempStorage.ps1
+++ DSC/SetPagefileonTempStorage.ps1
@@ -1,26 +1,13 @@
-configuration SetPageFile
-{
-      
-    Node localhost
-    {
-        
-        LocalConfigurationManager
-        {
-            RebootNodeIfNeeded = $true
-            ConfigurationMode = "ApplyOnly"
-        }
-        #Get LogicDisks for machine with Temporary Disks
-        $LogicalDisks = Get-WmiObject Win32_LogicalDisk | Where-Object VolumeName -EQ 'Temporary Storage'
+#Get LogicDisks for machine with Temporary Disks
+$LogicalDisks = Get-WmiObject Win32_LogicalDisk | Where-Object VolumeName -EQ 'Temporary Storage'
 
-        #Set Pagefile on Temp disk if exists
-        If ($LogicalDisks){
-            
-            Write-Host "Setting PageFile on $($LogicalDisks.DeviceID)"
-            $pagefileset = Get-WmiObject win32_pagefilesetting | Where-Object{$_.caption -like "$($LogicalDisks.DeviceID)*"}
-            $pagefileset.InitialSize = 6144
-            $pagefileset.MaximumSize = 24576
-            $pagefileset.Put() | Out-Null
+#Set Pagefile on Temp disk if exists
+If ($LogicalDisks){
+    
+    Write-Host "Setting PageFile on $($LogicalDisks.DeviceID)"
+    $pagefileset = Get-WmiObject win32_pagefilesetting | Where-Object{$_.caption -like "$($LogicalDisks.DeviceID)*"}
+    $pagefileset.InitialSize = 6144
+    $pagefileset.MaximumSize = 24576
+    $pagefileset.Put() | Out-Null
 
-        }
-    }
 }
