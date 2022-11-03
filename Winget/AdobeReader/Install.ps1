cd680936c9803d36b21874a5e88ec56cab456cc5
NKO <nkoadmin@7l4l1k.onmicrosoft.com>
2022-11-03 15:47:28 +0100

    EdittedInstallation

--- Winget/AdobeReader/Install.ps1
+++ Winget/AdobeReader/Install.ps1
@@ -1,8 +1,6 @@
-#Fill this variable with the Winget package ID
 $PackageName = "Adobe.Acrobat.Reader.64-bit"
 
-#Getting Current Installed Packages
-$AppxPackages = Get-AppxPackage
+$AppInstaller = Get-AppxProvisionedPackage -Online | Where-Object DisplayName -eq Microsoft.DesktopAppInstaller
 
 #Creating Loggin Folder
 if (!(Test-Path -Path C:\ProgramData\WinGetLogs)) {
@@ -11,111 +9,43 @@ if (!(Test-Path -Path C:\ProgramData\WinGetLogs)) {
 #Start Logging
 Start-Transcript -Path "C:\ProgramData\WinGetLogs\$($PackageName).log" -Append
 
-#Getting Windows Version
-$WindowsVersion = (Get-CimInstance Win32_OperatingSystem).version
+If($AppInstaller.Version -lt "2022.506.16.0") {
 
-#Start Winget Installation with dependencies if Windows 10 is detected
-If (($WindowsVersion -ge "10.0.18*") -and ($WindowsVersion -lt "10.0.22*")){
+    Write-Host "Winget is not installed, trying to install latest version from Github" -ForegroundColor Yellow
 
-    Write-Host "Host runs Windows 10, Checking if Winget needs to be installed." -ForegroundColor Yellow
-
-    If($AppxPackages.Name -notcontains "Microsoft.Winget.Source") {
-
-        Write-Host "Winget is not installed, trying to install latest version from Github" -ForegroundColor Yellow
-
-        Try {
-            #Create Temp Folder
-            Write-Host "Creating Temp Folder" -ForegroundColor Yellow
-
-            if (!(Test-Path -Path C:\Temp)) {
-            New-Item -Path C:\Temp -Force -ItemType Directory
-            }
-
-            Set-Location C:\temp
+    Try {
             
-            #Install Dependencies
-            If ($AppxPacakges.Name -notcontains "Microsoft.UI.Xaml.2.7"){
-                
-                try {
-                    Write-Host "Installing Microsoft.UI.Xaml.2.7" -ForegroundColor Yellow
-            
-                    Invoke-WebRequest -Uri "https://www.nuget.org/api/v2/package/Microsoft.UI.Xaml/2.7.0" -OutFile "C:\Temp\microsoft.ui.xaml.2.7.0.zip"
-                
-                    Expand-Archive C:\Temp\microsoft.ui.xaml.2.7.0.zip -Force
-                
-                    Add-AppxPackage "C:\Temp\microsoft.ui.xaml.2.7.0\tools\AppX\x64\Release\Microsoft.UI.Xaml.2.7.appx"
-                }
-                catch {
-                    Throw "Installing Microsoft.UI.Xaml.2.7 install Failed"
-                }
+        Write-Host "Creating Winget Packages Folder" -ForegroundColor Yellow
 
-            }
-            Else {
-                Write-Host "Microsoft.UI.Xaml.2.7 already installed" -ForegroundColor Green
-            }
-            #Install Dependencies
-            If ($AppxPacakges.Name -notcontains "Microsoft.VCLibs.140.00.UWPDesktop"){
-
-                try {
-                    Write-Host "Installing Microsoft.VCLibs.140.00.UWPDesktop" -ForegroundColor Yellow
-            
-                    Invoke-WebRequest -Uri "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx" -OutFile "C:\Temp\Microsoft.VCLibs.x64.14.00.Desktop.appx"
-                
-                    Add-AppxPackage "C:\Temp\Microsoft.VCLibs.x64.14.00.Desktop.appx"
-                }
-                catch {
-                    Throw "Microsoft.VCLibs.140.00.UWPDesktop"
-                }
-                
-
-            }
-            Else {
-                Write-Host "Microsoft.VCLibs.140.00.UWPDesktop already installed" -ForegroundColor Green
-            }
-            #Install Winget
-            If ($AppxPacakges.Name -notcontains "Microsoft.Winget.Source") {    
-                try {
-                    Write-Host "Installing Winget" -ForegroundColor Yellow  
-
-                    Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile "C:\Temp\Winget.msixbundle"
-    
-                    Add-AppxPackage "C:\Temp\Winget.msixbundle"
-                }
-                catch {
-                    Throw "Winget install Failed"
-                }     
-
-            }
-            Else {      
-                Write-Host "Winget already installed" -ForegroundColor Green
-            }
-
-            Set-Location C:\Windows
-
-            Write-Host "Cleaning up Temp Folder" -ForegroundColor Yellow
-
-            if (Test-Path -Path C:\Temp) {
-                Remove-Item -Path C:\Temp -Force -Recurse
-            }  
-        }
-        Catch {
-            Throw "Failed to install Winget"
-            Break
+        if (!(Test-Path -Path C:\ProgramData\WinGetPackages)) {
+            New-Item -Path C:\ProgramData\WinGetPackages -Force -ItemType Directory
         }
 
+        Set-Location C:\ProgramData\WinGetPackages
+
+        #Downloading Packagefiles
+        #Microsoft.UI.Xaml.2.7.0
+        Invoke-WebRequest -Uri "https://www.nuget.org/api/v2/package/Microsoft.UI.Xaml/2.7.0" -OutFile "C:\ProgramData\WinGetPackages\microsoft.ui.xaml.2.7.0.zip"
+        Expand-Archive C:\ProgramData\WinGetPackages\microsoft.ui.xaml.2.7.0.zip -Force
+        #Microsoft.VCLibs.140.00.UWPDesktop
+        Invoke-WebRequest -Uri "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx" -OutFile "C:\ProgramData\WinGetPackages\Microsoft.VCLibs.x64.14.00.Desktop.appx"
+        #Winget
+        Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile "C:\ProgramData\WinGetPackages\Winget.msixbundle"
+        #Installing dependencies + Winget
+        Add-ProvisionedAppxPackage -online -PackagePath:.\Winget.msixbundle -DependencyPackagePath .\Microsoft.VCLibs.x64.14.00.Desktop.appx,.\microsoft.ui.xaml.2.7.0\tools\AppX\x64\Release\Microsoft.UI.Xaml.2.7.Appx -SkipLicense
+
+        Write-Host "Starting sleep for Winget to initiate" -Foregroundcolor Yellow
+        Start-Sleep 5
     }
-    Else {
-        Write-Host "Winget already installed, moving on" -ForegroundColor Green
+    Catch {
+        Throw "Failed to install Winget"
+        Break
     }
+
+    }
+Else {
+    Write-Host "Winget already installed, moving on" -ForegroundColor Green
 }
-
-#Skipping Winget Installation if Windows 11 is detected
-If ($WindowsVersion -Like "10.0.22*"){
-
-    Write-Host "Host runs Windows 11, skipping installation of WinGet" -ForegroundColor Green
-
-}
-
 #Trying to install Package with Winget
 IF ($PackageName){
     try {
@@ -127,7 +57,6 @@ IF ($PackageName){
     }
 }
 Else {
-    Write-Host "No PackageName specified" -ForegroundColor Red
+    Write-Host "Package $($PackageName) Already Installed" -ForegroundColor Yellow
 }
-#Stop Logging
 Stop-Transcript
