6554ca0b13c6fa93e385262f6f3735d3da83ea24
NKO <nkoadmin@7l4l1k.onmicrosoft.com>
2022-11-03 16:19:26 +0100

    UpdatedUninstallation

--- Winget/AdobeReader/Detection.ps1
+++ Winget/AdobeReader/Detection.ps1
@@ -1,7 +1,15 @@
 #Fill this variable with the Winget package ID
 $PackageName = "Adobe.Acrobat.Reader.64-bit"
 
-$InstalledApps = winget list --id $PackageName
+$ResolveWingetPath = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
+if ($ResolveWingetPath){
+    $WingetPath = $ResolveWingetPath[-1].Path
+}
+
+$config
+cd $wingetpath
+
+$InstalledApps = .\winget.exe winget list --id $PackageName
 
 if ($InstalledApps) {
     Write-Host "$($PackageName) is installed"
--- Winget/AdobeReader/Uninstall.ps1
+++ Winget/AdobeReader/Uninstall.ps1
@@ -15,8 +15,16 @@ if ($InstalledApps) {
     
     Write-Host "Trying to uninstall $($PackageName)"
     
-    try {
-        winget uninstall $PackageName --silent
+    try {        
+        $ResolveWingetPath = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
+        if ($ResolveWingetPath){
+            $WingetPath = $ResolveWingetPath[-1].Path
+        }
+
+        $config
+        cd $wingetpath
+
+        .\winget.exe uninstall $PackageName --silent
     }
     catch {
         Throw "Failed to uninstall $($PackageName)"
