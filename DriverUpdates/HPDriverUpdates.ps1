db2b605b9b4bbbac78259e9caf3a820af2b3c78f
NielsKok <Ruthhl3ss>
2024-01-03 17:48:16 +0100

    Add HP driver update script with necessary modules and dependencies

--- DriverUpdates/HPDriverUpdates.ps1
+++ DriverUpdates/HPDriverUpdates.ps1
@@ -1,6 +1,10 @@
 Start-Transcript -Path C:\Windows\Temp\HPIA.log
 
+Set-ExecutionPolicy Bypass -Force
+
 Install-PackageProvider -name NuGet -force
+
+Import-Module -Name PowershellGet
 
 Install-Module -Name PowershellGet -Force
 
@@ -20,8 +24,20 @@ If (!(Test-Path -Path $UpdatePath)){
 
 Install-HPImageAssistant -Extract -DestinationPath $HPImageAssistantExtractPath -ErrorAction Stop
 
-$ImageAssistant = Get-Childitem -Path C:\Windows\Temp\HPIA -Recurse | Where-Object name -Like "HPImageAssistant.exe*"
+#$ImageAssistant = Get-Childitem -Path C:\Windows\Temp\HPIA -Recurse | Where-Object name -Like "HPImageAssistant.exe*"
 
-Start-Process -FilePath $($ImageAssistant.fullname) -ArgumentList "/Operation:Analyze /Action:Install /Selection:All /Silent /Category:Drivers,Software /ReportFolder:'C:\Windows\Temp' /SoftpaqDownloadFolder:$($UpdatePath)"
+C:\Windows\Temp\HPIA\HPImageAssistant.exe /Operation:Analyze /Action:Install /Selection:All /Silent /Category:Drivers,Software /ReportFolder:'C:\Windows\Temp' /SoftpaqDownloadFolder:$($UpdatePath)
+
+$Process = Get-Process -Name HPImageAssistant -ErrorAction SilentlyContinue
+
+Do {
+    
+    Write-Output "HP ImageAssistant is still running"
+    Start-Sleep 30
+
+    $Process = Get-Process -Name HPImageAssistant -ErrorAction SilentlyContinue
+    
+}
+While ($Process -ne $null)
 
 Stop-Transcript
