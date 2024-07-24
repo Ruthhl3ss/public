2fe44aa4c91057c5b0d3cf8feac7001d4e1aec12
NielsKok <Ruthhl3ss>
2024-07-24 09:55:16 +0200

    NewRemediations

---
+++ Remediations/FSLogixLocalAdminExclude_Detect.ps1
@@ -0,0 +1,37 @@
+Start-Transcript -Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\FSLogixExclude_Detect.log" -Append
+
+$Groups = @(
+    "FSLogix ODFC Exclude List"
+    "FSLogix Profile Exclude List"
+)
+
+$AdminToExclude = "NKOAdmin"
+
+$Count = 0
+
+foreach ($Group in $Groups){
+
+    #$Object = Get-WmiObject -Query "Select * From Win32_Group" | Where-Object name -eq $Group
+
+    Write-Output "Checking group $Group"
+
+    $Query = net localgroup $($Group)
+
+    $Members = $Query[6..($Query.Length-3)]
+
+    if ($Members -notcontains $AdminToExclude) {
+        Write-Output "User: $AdminToExclude is not member, adding to count"
+        $Count++
+    }
+
+}
+
+if ($Count -ge 1) {
+    Write-Output "User: $AdminToExclude need to be added to the exclude groups"
+    Exit 1
+}
+else {
+    Write-Output "User: $AdminToExclude is already member of both groups"
+}
+
+Stop-Transcript
---
+++ Remediations/FSLogixLocalAdminExclude_Remediate.ps1
@@ -0,0 +1,29 @@
+Start-Transcript -Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\FSLogixExclude_Remediate.log" -Append
+
+$Groups = @(
+    "FSLogix ODFC Exclude List"
+    "FSLogix Profile Exclude List"
+)
+
+$AdminToExclude = "NKOAdmin"
+
+foreach ($Group in $Groups){
+
+    #$Object = Get-WmiObject -Query "Select * From Win32_Group" | Where-Object name -eq $Group
+
+    Write-Output "Checking group $Group"
+
+    $Query = net localgroup $($Group)
+
+    $Members = $Query[6..($Query.Length-3)]
+
+    if ($Members -notcontains $AdminToExclude) {
+        Write-Output "User: $AdminToExclude is not member, adding to group"
+        
+        net localgroup $Group $AdminToExclude /add
+
+    }
+
+}
+
+Stop-Transcript
